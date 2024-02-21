0636f70dac09860ffdbd42f4cf342b2a18c5eefd
ed Sheeran <99225359+sircookielmoa@users.noreply.github.com>
2024-02-21 16:57:50 +1100

    Commit 21/02/24

--- StringClass/String.cpp
+++ StringClass/String.cpp
@@ -1,24 +1,26 @@
 #include "String.h"
+#include <cctype>
 #include <cstring>
+#include <iostream>
 
 String::String()
+	: str(nullptr)
 {
-	null = 0;
-	str = new char[0];
-	length = 0;
+	str = new char[1] {'\0'};
 }
 
 String::String(const char* _str)
 	: str(nullptr)
 {
-	//1. Figure out how much sext _str is pointing at
+	//1. Figure out how much text _str is pointing at
 	std::size_t _strLen = std::strlen(_str) + 1;
-	//2. Make str point at an arrau big enough to fit the text in _str
+	//2. Make str point at an array big enough to fit the text in _str
 	str = new char[_strLen + 1];
 	//3. Copy the text from _str to str.
 	std::strncpy(str, _str, _strLen);
 }
 
+//copy constructor
 String::String(const String& _other)
 	: str(nullptr)
 {
@@ -28,4 +30,44 @@ String::String(const String& _other)
 String::~String()
 {
 	delete[] str;
+}
+
+size_t String::Length() const
+{
+	return std::strlen(str);
+}
+
+char& String::CharacterAt(size_t _index)
+{
+	if (_index > Length())
+	{
+		_index = Length();
+	}
+
+	return str[_index];
+}
+
+const char* String::CStr() const
+{
+	return str;
+}
+
+String& String::ToLower()
+{
+	std::tolower;
+
+	return *this;
+}
+
+String& String::ToUpper()
+{
+	std::toupper;
+
+	return* this;
+}
+
+String& String::WriteToConsole()
+{
+	std::cout << str << std::endl;
+	return *this;
 }
--- StringClass/StringClass.cpp
+++ StringClass/StringClass.cpp
@@ -9,6 +9,14 @@ int main()
 		//std::string s = ";
 	//boom
 		//std::cout << s << std::endl;
-	String s = "Hello, World!";
+	String s = "POOPy bumhole";
+	String s2 = s;
+
+	s.WriteToConsole();
+	s.ToLower();
+	s.WriteToConsole();
+	//s.Append(" again. ");
+
+	std::cout << s.CharacterAt(1) << std::endl;
 	std::cout << s.CStr() << std::endl;
 	}
