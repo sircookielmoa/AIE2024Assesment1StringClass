2253308930cd5e0cbac40eecbd29f7401ce7cd27
ed Sheeran <99225359+sircookielmoa@users.noreply.github.com>
2024-03-01 16:07:48 +1100

    Commit 01/03/24

--- StringClass/StringClass.cpp
+++ StringClass/StringClass.cpp
@@ -6,42 +6,44 @@
 
 int main()
 	{
-		//std::string s = ";
-	//boom
+		//std::string s = "";
 		//std::cout << s << std::endl;
 	
-	String s1 = "";
-	String s2 = "";
-	
-	s1.ReadFromConsole();
-	s2.ReadFromConsole();
+		String s1 = "";
+		String s2 = "";
+		
+		s1.ReadFromConsole();
+		s2.ReadFromConsole();
+		
+		s1.Append(s2);
+		s1.ToLower();
+		
+		s1.WriteToConsole();
+		
+		//std::cout << s1.CharacterAt(12) << std::endl;
 
-	s1.Append(s2);
-	s1.ToLower();
-	
-	s1.WriteToConsole();
+		//std::size_t everyonePos = s1.Find(0, "stinky");
+		s2.ToLower();
+		s1.Append(s2);
 
-	std::cout << s1.CharacterAt(12) << std::endl;
+		std::cout << s1.CStr() << std::endl;
+		//std::cout << everyonePos << std::endl;
 
-	//std::size_t everyonePos = s1.Find(0, "stinky");
-	//s2.ToLower();
-	//s1.Append(s2);
+		s1.ReadFromConsole();
 
-	//std::cout << s1.CStr() << std::endl;
-	//std::cout << everyonePos << std::endl;
+		s1.WriteToConsole();
+		s2.WriteToConsole();
+		s1.ToUpper();
 
-	//s.ReadFromConsole();
+		//std::cout << pageBreak.CStr() << std::endl;
 
-	//s.WriteToConsole();
-	//s2.WriteToConsole();
-	//s.ToUpper();
+		std::cout << std::boolalpha;
+		std::cout << s1.EqualTo(s2) << std::endl;
 
-	//std::cout << pageBreak.CStr() << std::endl;
+		std::cout << s1.CharacterAt(20) << std::endl;
+		std::cout << s1.CStr() << std::endl;
+		std::cout << s2.CStr() << std::endl;
 
-	//std::cout << std::boolalpha;
-	//std::cout << s.EqualTo(s2) << std::endl;
 
-	//std::cout << s.CharacterAt(20) << std::endl;
-	//std::cout << s.CStr() << std::endl;
-	//std::cout << s2.CStr() << std::endl;
+
 	}
